fa1be4ee56494ea416cda5139d67642188f139a7
Ramsey <razure@uncc.edu>
2022-02-12 21:17:33 -0500

    54

--- itis3135/scripts/defaultsscript.js
+++ itis3135/scripts/defaultsscript.js
@@ -40,10 +40,9 @@ function calcAvg()
     let hours1 = parseInt(document.getElementById('hours1').value) || 0;
     let hours2 = parseInt(document.getElementById('hours2').value) || 0;
     let hours3 = parseInt(document.getElementById('hours3').value) || 0;
-    let hours4 = parseInt(document.getElementById('hours4').value) || 0;
-    let hours5 = parseInt(document.getElementById('hours5').value) || 0;
+   
 
-    let hoursArray = new Array(hours1, hours2, hours3, hours4, hours5);
+    let hoursArray = new Array(hours1, hours2, hours3);
     let numWorkers = 0;
     for(var i = 0; i < 5; i++) 
     {
@@ -52,12 +51,6 @@ function calcAvg()
             numWorkers++;
         }
     }
-    document.getElementById('output3').innerHTML = "Average work time is: " + ((hours1 + hours2 + hours3 + hours4 + hours5) / numWorkers) + " hours.";
+    document.getElementById('output3').innerHTML = "Average work time is: " + ((hours1 + hours2 + hours3) / numWorkers) + " hours.";
 }
 
-function modularCalc()
-{
-    let jackals = document.getElementById('jackals').value;
-    let cookies = document.getElementById('cookies').value;
-    document.getElementById('output4').innerHTML = "Remaining cookies: " + (cookies % jackals) + ".";
-}
