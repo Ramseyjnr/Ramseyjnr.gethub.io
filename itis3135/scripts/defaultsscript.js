77668d19d7d90038129ae372a8151be1b9fd2bbe
Ramsey <razure@uncc.edu>
2022-02-12 22:23:31 -0500

    444

--- itis3135/scripts/defaultsscript.js
+++ itis3135/scripts/defaultsscript.js
@@ -3,33 +3,8 @@ function scriptTest()
     alert("My script is running.");
 }
 
-function printTime() 
-{
-    var today = new Date();
-    var year = now.getFullYear();
-    var month = today.getMonth();
-    var day = now.getDay();
-    var date = now.getDate();
-    var hour = now.getHours();
-    var min = now.getMinutes();
-    var am_pm;
 
-    var month = new Array('January', 'February', 'March', 'April', 'May', 'June', 
-    'July', 'August', 'September', 'October', 'November', 'December');
 
-    var mtwtfss = new Array('Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 
-    'Sunday');
-
-    if(hour >= 12){
-        hour %=12;
-        am_pm;
-    }else {
-        am_pm="am";
-    }
-
-    document.getElementById('printTime').innerHTML = "Current time is: " + today.getHours() + ":" +
-     today.getMinutes() + ".  Today is: " + monthWord[month] + " " + today.getDate() + ", " + today.getFullYear() + ".";
-}
 
 
 
@@ -38,8 +13,8 @@ function printTime()
 
 function formOutput()
 {
-    let name = document.getElementById('name').value;
-    let feeling = document.getElementById('feeling').value;
+    var name = document.getElementById('name').value;
+    var feeling = document.getElementById('feeling').value;
     document.getElementById("output").innerHTML = `WhitRams designs Welcome You , ${name}!  We're glad you are doing ${feeling}!`;
 }
 
@@ -53,13 +28,13 @@ function addSum()
 
 function calcAvg()
 {
-    let hours1 = parseInt(document.getElementById('hours1').value) || 0;
-    let hours2 = parseInt(document.getElementById('hours2').value) || 0;
-    let hours3 = parseInt(document.getElementById('hours3').value) || 0;
+    var hours1 = parseInt(document.getElementById('hours1').value) || 0;
+    var hours2 = parseInt(document.getElementById('hours2').value) || 0;
+    var hours3 = parseInt(document.getElementById('hours3').value) || 0;
    
 
-    let hoursArray = new Array(hours1, hours2, hours3);
-    let numWorkers = 0;
+    var hoursArray = new Array(hours1, hours2, hours3);
+    var numWorkers = 0;
     for(var i = 0; i < 5; i++) 
     {
         if(hoursArray[i] != 0) 
