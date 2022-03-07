612af85496f7bf42ce190620779e1361dc19d995
Ramsey <razure@uncc.edu>
2022-03-06 20:11:45 -0500

    vvv

--- itis3135/scripts/defaultsscript.js
+++ itis3135/scripts/defaultsscript.js
@@ -3,6 +3,34 @@ function scriptTest()
     alert("My script is running.");
 }
 
+function showDateTime() 
+{
+    var current = new Date();
+    var monthNumber = current.getMonth() + 1;
+    var day = current.getDay();
+    var year = current.getFullYear();  
+    var date = current.getDate();
+    var hour = current.getHours();
+    var minute = current.getMinutes();
+    var dayOrNight;
+
+    var monthName = new Array("January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December");
+    var dayOfTheWeek = new Array("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday");
+
+    if(hour >= 12)
+    {
+        hour %= 12;
+        dayOrNight = "pm";
+    }
+    else
+    {
+        dayOrNight = "am";
+    }
+
+    let fullTimeDate = "Today is " + hour + ":" + minute + dayOrNight + " on " + dayOfTheWeek[day] + ", " + date + " " + monthName[monthNumber] + " " + year;
+    document.getElementById("timeoutput").innerHTML = fullTimeDate;
+}
+
 function formOutput()
 {
     var name = document.getElementById('name').value;
