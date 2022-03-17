fbd6c9cac5de432702ebbe1416f95a8823627d4a
Ramsey <razure@uncc.edu>
2022-03-17 17:32:56 -0400

    333

---
+++ itis3135/project/scripts/script.js
@@ -0,0 +1,7 @@
+// method for accordion widget
+$(document).ready(function() {
+   $("#accordion").accordion({
+      collapsible: true, // to show collapse
+      active: false // active
+   });
+});
