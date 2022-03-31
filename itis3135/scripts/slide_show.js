e667f4a5e936961aeb7091f7403892b65964ec7a
Ramsey <razure@uncc.edu>
2022-03-31 16:25:22 -0400

    ww

--- itis3135/scripts/slide_show.js
+++ itis3135/scripts/slide_show.js
@@ -1,45 +1,30 @@
-const $ = function(id) 
-{
-    return document.getElementById(id);
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
 }
 
-document.addEventListener("DOMContentLoaded", () => 
-{
-    //caption h2 element
-    const caption = $("caption");
-    //img element to show
-    const mainImage = $("main-image");
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
 
-    //get a tags in ul
-    const links = $("image-list").getElementsByTagName('a');
-
-    //process image links
-    const imageCache = [];
-    let image = null;
-
-    for (let link of links)
-    {
-        image = new Image();
-        image.src = link.href;
-        image.alt = link.title;
-
-        imageCache[imageCache.length] = image;
-    }
-
-    //Slide show
-    let imageCounter = 0;
-    setInterval( () => 
-    {
-        //Get current image index
-        imageCounter = (imageCounter + 1) % imageCache.length;
-
-        //Get image from array
-        image = imageCache[imageCounter];
-
-        mainImage.src = image.src;
-        mainImage.alt = image.alt;
-        caption.textContent = image.alt;
-    }, 3000); //3 Second interval 
-
-
-})
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("demo");
+  let captionText = document.getElementById("caption");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  captionText.innerHTML = dots[slideIndex-1].alt;
+}
