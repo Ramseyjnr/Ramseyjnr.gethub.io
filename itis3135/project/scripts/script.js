05dd7bbbc31055d734e558224a633f30655c6dd9
Ramsey <razure@uncc.edu>
2022-04-04 16:42:35 -0400

    tt

--- itis3135/project/scripts/script.js
+++
@@ -1,7 +0,0 @@
-// method for accordion widget
-$(document).ready(function() {
-   $("#accordion").accordion({
-      collapsible: true, // to show collapse
-      active: false // active
-   });
-});
--- itis3135/project/scripts/script3.js
+++
@@ -1,7 +0,0 @@
-// method for slideshow plugin
-$(document).ready(function() {
-    $('#slideshow').cycle({
-        fx: 'fade' // fade picture slideshow
-    });
-
-});
