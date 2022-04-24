a92e79619064e0cef1948921d7f6385d4176525c
Ramsey <razure@uncc.edu>
2022-04-24 18:49:48 -0400

    bbb

---
+++ itis3135/project/scripts/indexs.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+    $.ajax({
+        type: "get",
+        url: "aboutus.json",
+        beforeSend: function () {
+            $("#about").html("Loading...");
+        },
+        timeout: 10000,
+        error: function (xhr, status, error) {
+            alert("Error: " + xhr.status + " - " + error);
+        },
+        dataType: "json",
+        success: function (data) {
+            $("#about").html("");
+
+            $.each(data, function () {
+                $.each(this, function (key, value) {
+                    $("#about").append(
+
+                        "<h1>" + value.title+ "</h1><p>"+ value.info+"</p>"
+
+                        );
+
+                });
+            });
+
+
+        },
+    });
+});
+
